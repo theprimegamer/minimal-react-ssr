fa05f0fc8cda4e99b8549e9f2aff2fb7de07f2f0
theprimegamer <theprimegamer@gmail.com>
2022-09-27 20:32:07 -0400

    Using stream now

---
+++ src/common/components/AppContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { App } from "./App";
+
+export default function AppContainer() {
+  return (
+    <div id="appRoot">
+      <App />
+    </div>
+  );
+}
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import express from "express";
 import ReactDOMServer from "react-dom/server";
-import { App } from "src/common/components/App";
 import path from "path";
+import AppContainer from "src/common/components/AppContainer";
 
 const app = express();
 const port = 3000;
@@ -10,11 +10,12 @@ const port = 3000;
 app.use("/static", express.static(path.resolve(__dirname, "public")));
 
 app.get("/", (_req, res) => {
-  const html = `<div id="appRoot">${ReactDOMServer.renderToString(
-    <App />
-  )}</div><script src="/static/app-load.js"></script>`;
-
-  res.send(html);
+  const stream = ReactDOMServer.renderToPipeableStream(<AppContainer />, {
+    bootstrapScripts: ["/static/app-load.js"],
+    onShellReady: () => {
+      stream.pipe(res);
+    },
+  });
 });
 
 app.listen(port, () => {
